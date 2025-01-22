e9e9f6b1aeef099222aba61357e94c4fdd06ed52
faldo <pn4066283@gmail.com>
2025-01-22 09:01:08 +0700

    done aja nih

--- src/components/NavbarComponents.jsx
+++ src/components/NavbarComponents.jsx
@@ -28,7 +28,7 @@ const NavbarComponents = () => {
   const [openMenu, setOpenMenu] = useState(true);
   const [closeDiscount, setCloseDiscount] = useState(false);
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col relative z-50">
       <div className={`bg-slate-900 text-white p-2 text-center z-20 items-center justify-center w-full ${closeDiscount ? 'hidden' : "flex"}`}>
         <p className="text-sm font-semibold">
           Sign up GET 20% OFF for your first order.{" "}
@@ -37,7 +37,7 @@ const NavbarComponents = () => {
       </div>
       <div className="max-w-7xl m-auto p-5 w-full flex justify-between items-center">
         <div className="z-20">
-          <h1 className="text-xl font-bold text-slate-800">RUNDSHOP</h1>
+          <h1 className="text-xl font-bold text-slate-800 lg:text-2xl">RUNDSHOP</h1>
         </div>
         <div
           className={`md:hidden fixed flex flex-col  right-0 w-full min-h-screen bg-white items-center justify-center gap-3 ${
@@ -56,7 +56,7 @@ const NavbarComponents = () => {
         </div>
         <div className="hidden md:flex items-center gap-4">
           {menu.map((item) => (
-            <a key={item.name} href={item.link} className="text-slate-800 text-base font-normal">
+            <a key={item.name} href={item.link} className="text-slate-800 text-base font-normal lg:text-lg">
               {item.name}
             </a>
           ))}
